0cc75fedd2a92901674a0c213786672da1b9b76b
rladl <rladlsdks12@gmail.com>
2026-02-13 14:24:55 +0900

    Initial commit

---
+++ src/Q1/StudentInfo.java
@@ -0,0 +1,18 @@
+package Q1;
+
+public class StudentInfo {
+    public static void main() {
+        String name = "김인안";
+        int age = 26;
+        double height = 168.5;
+        char grade = '2';
+        boolean isStudent = false;
+
+        System.out.println("=== 학생 정보 ===");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade + "학년");
+        System.out.println(isStudent);
+    }
+}
